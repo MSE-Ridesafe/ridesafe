9329fdb62081bcf956e7bb3ead1c02d2e927b436
Dustin Hoffmann <dustin_9@outlook.de>
2026-06-11 14:57:43 +0200

    Initial commit.

---
+++ app/src/main/java/de/uhi/enia/ridesafe/MainActivity.kt
@@ -0,0 +1,92 @@
+package de.uhi.enia.ridesafe
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.adaptive.navigationsuite.NavigationSuiteScaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.PreviewScreenSizes
+import de.uhi.enia.ridesafe.ui.theme.RidesafeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            RidesafeTheme {
+                RidesafeApp()
+            }
+        }
+    }
+}
+
+@PreviewScreenSizes
+@Composable
+fun RidesafeApp() {
+    var currentDestination by rememberSaveable { mutableStateOf(AppDestinations.DASHBOARD) }
+
+    NavigationSuiteScaffold(
+        navigationSuiteItems = {
+            AppDestinations.entries.forEach {
+                item(
+                    icon = {
+                        Icon(
+                            painterResource(it.icon),
+                            contentDescription = it.label
+                        )
+                    },
+                    label = { Text(it.label) },
+                    selected = it == currentDestination,
+                    onClick = { currentDestination = it }
+                )
+            }
+        }
+    ) {
+        Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+            Greeting(
+                name = "Android",
+                modifier = Modifier.padding(innerPadding)
+            )
+        }
+    }
+}
+
+enum class AppDestinations(
+    val label: String,
+    val icon: Int,
+) {
+    DASHBOARD("Dashboard", R.drawable.ic_home),
+    RIDES("Rides", R.drawable.ic_favorite),
+    GARAGE("Garage", R.drawable.ic_account_box),
+    SETTINGS("Settings", R.drawable.ic_launcher_foreground)
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    RidesafeTheme {
+        Greeting("Android")
+    }
+}
