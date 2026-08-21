c7a6cafe67976062c48a91fb58f18401f1f776f5
Dustin Hoffmann <dustin_9@outlook.de>
2026-08-21 16:45:59 +0200

    Add missing test implementation to make gradle build successful again.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -76,6 +76,7 @@ dependencies {
     implementation(libs.maps.utils)
     implementation(libs.commons.math3)
     testImplementation(libs.junit)
+    androidTestImplementation(platform(libs.androidx.compose.bom))
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(libs.androidx.junit)
