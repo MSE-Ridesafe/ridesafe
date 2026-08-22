400ee9fb7bbc25966a9367ce40326b71be3bf058
NormalErik <kasten-erik@web.de>
2026-08-22 14:34:08 +0200

    fix: add test and fix lint

--- app/src/main/java/de/uhi/enia/ridesafe/ui/screens/rides/RidesScreen.kt
+++ app/src/main/java/de/uhi/enia/ridesafe/ui/screens/rides/RidesScreen.kt
@@ -5,6 +5,7 @@ package de.uhi.enia.ridesafe.ui.screens.rides
 import android.net.Uri
 import android.util.Log
 import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.combinedClickable
 import androidx.compose.foundation.layout.Arrangement
@@ -16,7 +17,6 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.background
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
@@ -72,8 +72,8 @@ import de.uhi.enia.ridesafe.util.formatDuration
 import de.uhi.enia.ridesafe.util.formatDurationMs
 import de.uhi.enia.ridesafe.util.formatTimeOfDay
 import de.uhi.enia.ridesafe.util.rideDay
-import java.time.LocalDate
 import kotlinx.coroutines.launch
+import java.time.LocalDate
 
 @Composable
 fun RidesScreen(
@@ -137,11 +137,15 @@ fun RidesScreen(
                 }
                 onExportResultConsumed()
             }
+
             RideExportState.Error -> {
                 snackbarHostState.showSnackbar(exportErrorMessage)
                 onExportResultConsumed()
             }
-            RideExportState.Idle, RideExportState.Exporting -> Unit
+
+            RideExportState.Idle, RideExportState.Exporting -> {
+                Unit
+            }
         }
     }
 
